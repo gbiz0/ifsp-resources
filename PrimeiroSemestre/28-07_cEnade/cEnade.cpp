7294ac810dab4c538531c62a7e5c3ce6c3b68ca8
gbiz0 <jardimbgustavo@gmail.com>
2023-08-04 09:08:54 -0300

    core: initial commit

---
+++ PrimeiroSemestre/28-07_cEnade/cEnade.cpp
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.cc to edit this template
+ */
+
+/* 
+ * File:   cEnade.cpp
+ * Author: jardi
+ * 
+ * Created on 1 de Agosto de 2023, 19:49
+ */
+
+#include "cEnade.h"
+
+cEnade::cEnade() {
+}
+
+cEnade::cEnade(const cEnade& orig) {
+}
+
+cEnade::~cEnade() {
+}
+
---
+++ PrimeiroSemestre/28-07_cEnade/cEnade.h
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.h to edit this template
+ */
+
+/* 
+ * File:   cEnade.h
+ * Author: jardi
+ *
+ * Created on 1 de Agosto de 2023, 19:49
+ */
+
+#ifndef CENADE_H
+#define CENADE_H
+
+class cEnade {
+public:
+    cEnade();
+    cEnade(const cEnade& orig);
+    virtual ~cEnade();
+private:
+
+};
+
+#endif /* CENADE_H */
+
