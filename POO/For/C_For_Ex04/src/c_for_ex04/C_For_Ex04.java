4bbf8fae00047e54b8c701f11d0cb38a9a6c799b
gbiz0 <jardimbgustavo@gmail.com>
2023-08-31 09:45:04 -0300

    feat: add new For exercise

--- POO/For/C_For_Ex04/src/c_for_ex04/C_For_Ex04.java
+++ POO/For/C_For_Ex04/src/c_for_ex04/C_For_Ex04.java
@@ -14,7 +14,10 @@ public class C_For_Ex04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Altura altura = new Altura();
+        
+        altura.calcular();
+        altura.show();
     }
     
 }
