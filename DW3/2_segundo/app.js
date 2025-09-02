777f4d1661a3e77807a3935b56cc7ee66f38b0ac
gbiz0 <jardimbgustavo@gmail.com>
2025-09-02 09:51:56 -0300

    fix: applying adjustments

--- DW3/2_segundo/app.js
+++ DW3/2_segundo/app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = require("./routes/route");
 
 const app = express();
-const port = 40000;
+const port = 3000;
 app.use(express.json());
 app.use(router);
 
 app.listen(port, () => {
-  console.log("App listening at port ${port}");
+  console.log(`App listening at port ${port}`);
 });
